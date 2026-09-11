d13145f71bf213f4aa0348a143bfcdffccbd9009
Daffa Nadua <naduaraishad@gmail.com>
2026-09-11 15:21:00 +0700

    Implement basic MVC structure

---
+++ resources/views/members/index.blade.php
@@ -0,0 +1,2 @@
+<h1>Daftar Member</h1>
+<p>Daffa Nadua Raishad</p>
--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\MemberController;
+
+Route::get('/books', [BookController::class, 'index']);
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::get('/members', [MemberController::class, 'index']);
 
 Route::get('/', function () {
     return view('welcome');
